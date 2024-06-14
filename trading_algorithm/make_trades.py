5b35af6f1ffbd31da4501062cc1d9812802a8909
colurw <colurw@gmail.com>
2024-06-14 19:15:54 +0100

    bug fixes

--- trading_algorithm/make_trades.py
+++ trading_algorithm/make_trades.py
@@ -7,6 +7,7 @@ import sys
 import pyttsx3
 from dotenv import load_dotenv
 exec( open('trading_algorithm/calculate_levels_private.py').read() )   # allows global vars (necessary with schedule()) to be updated
+from time import localtime, gmtime, strftime
 
 
 def get_streamed_data():
@@ -23,7 +24,7 @@ def get_streamed_data():
 
     except:
         log_message('streamer failed')
-        raise Exception('streamer failed!')
+        raise Exception('streamer failed')
         # sys.exit('streamer failed') 
     
 
@@ -35,13 +36,11 @@ def check_connection(timestamp):
 
     with open('trading_algorithm/persisted/timestamp.txt', 'w') as file:
         file.write(timestamp)
-
-    print(f'\n{str(last_timestamp)}\n{str(timestamp)}')
     
     if timestamp == last_timestamp:
-        log_message(f'stream not updating')
-        print('stream not updating!')
-        # sys.exit('stream not updating')     
+        log_message(f'stream repeated')
+        raise Exception('stream repeated')
+        # sys.exit('stream repeated')     
         
 
 def calculate_order_size(margin, max_risk, price, stop_level):
@@ -69,35 +68,52 @@ def clear_book(client):
         print('no open stops')
 
 
-def log_message(words, text2speech=True):
+def log_message(string, text2speech=True, print=True):
+
+    time = strftime("%Y-%m-%d %H:%M:%S", localtime())  # also gmtime()
 
     with open('trading_algorithm/persisted/log.txt', 'a') as file:
-        file.write(words +'\n') 
+        file.write(f'{time} {string} \n') 
     
     if text2speech == True:
         engine = pyttsx3.init()
         voices = engine.getProperty('voices')
         engine.setProperty('voice', voices[0].id) 
-        engine.say(words)
+        engine.say(string)
         engine.runAndWait()
     
 
+def get_trade_status(client):
+    quantity = client.Position.Position_get(filter='{"symbol": "XBTUSD"}').result()[0][0]['currentQty']
+    status = 'none'
+    
+    if quantity > 0:
+        status = 'in_long'
+
+    elif quantity < 0:
+        status = 'in_short'
+
+    return status
+
+
 MAX_RISK = 3
-FRESH_START = True   # for testing only 
+FRESH_START = True   
+DELAY = 3
 
 load_dotenv()   # get env vars from .env file
+log_message('start up')
 
 client = bitmex.bitmex(test=True, 
                        api_key=os.getenv('TESTNET_KEY'), 
                        api_secret=os.getenv('TESTNET_SECRET'))   
 
-with open('trading_algorithm/persisted/trade_status.txt', 'r') as file:   # persisted_data needs volume mount in container
-    trade_status = file.read()
+trade_status = get_trade_status(client)    
 
 if FRESH_START:
-    clear_book(client)
     trade_status == 'none'
-
+    clear_book(client)
+    log_message('fresh start')
+    
 if trade_status != 'none':
 
     with open('trading_algorithm/persisted/trailing_stop.txt', 'r') as file: 
@@ -113,17 +129,22 @@ lower_bound = 0.0
 
 calculate_levels(graph=True) # type: ignore
 
-# get bucketed historic data and calculate significant levels on turn of each hour
-schedule.every().minute.at(':00').do(calculate_levels, graph=True)   # type: ignore   # every minute!!
-# schedule.every(5).minutes.do(calculate_levels, graph=True)   
+schedule.every(5).minutes.at(':00').do(calculate_levels, graph=True)   # type: ignore   # every 5 minutes!!
 
 while True:
-    schedule.run_pending()   
-    time.sleep(3)
+    schedule.run_pending()   # recalculate levels every every new candle - check EC2 time sync method
+    time.sleep(DELAY)
+
+    trade_status = get_trade_status(client)  
 
     # get current prices every second
-    last_price, lowest_ask, highest_bid, timestamp = get_streamed_data()
-    
+    last_price, lowest_ask, highest_bid, timestamp = get_streamed_data()   # highest/lowest necessary?
+    print('UB = ', int(upper_bound))
+    print('LB = ', int(lower_bound))
+    print('LT = ', int(long_trigger)) 
+    print('LP = ', int(last_price)) 
+    print('ST = ', int(short_trigger))
+    print()
     # os.system('cls')   
     check_connection(timestamp)
 
@@ -131,24 +152,29 @@ while True:
 
         # open long position
         clear_book(client)
-        margin = client.User.User_getMargin(currency='XBt').result()[0]['amount']
+        margin = client.User.User_getMargin(currency='XBt').result()[0]['amount']    # must set account to cross margin
         size = calculate_order_size(margin, MAX_RISK, last_price, lower_bound)
         client.Order.Order_new(symbol='XBTUSD', 
                                ordType='Market', 
                                side='Buy', 
                                orderQty=size).result()
         trade_status = 'in_long'
+        log_message(f'{last_price} {size} long trade opened')
 
         # set long stop
-        liquidation_price = client.Position.Position_get().result()[0][0]['liquidationPrice']
-        trailing_stop = max(lower_bound, float(liquidation_price) * 1.1)   
+        time.sleep(3)
+        liquidation_price = client.Position.Position_get(filter='{"symbol": "XBTUSD"}').result()[0][0]['liquidationPrice']
+        trailing_stop = max(lower_bound, float(liquidation_price) * 1.1) 
+        print('LB = ', int(lower_bound))
+        print('LIQP = ', int(liquidation_price))  
+        print('STOPPX = ', int(trailing_stop))
         client.Order.Order_new(symbol='XBTUSD', 
                                ordType='Stop', 
                                orderQty=-size, 
                                stopPx=int(trailing_stop), 
                                execInst='MarkPrice', 
                                clOrdID='long_stop').result()
-        log_message(f'{last_price} {size} long trade opened')
+        log_message(f'{int(trailing_stop)} long stop set')
         
     if last_price < short_trigger and trade_status == 'none':
 
@@ -161,17 +187,23 @@ while True:
                                side='Sell', 
                                orderQty=-size).result()  
         trade_status = 'in_short'
+        log_message(f'{last_price} {size} short trade opened')
 
         # set short stop
-        liquidation_price = client.Position.Position_get().result()[0][0]['liquidationPrice']
+        time.sleep(3)
+        liquidation_price = client.Position.Position_get(filter='{"symbol": "XBTUSD"}').result()[0][0]['liquidationPrice']
         trailing_stop = min(upper_bound, float(liquidation_price) / 1.1) 
+        print('UB = ', int(upper_bound))
+        print('LIQP = ', int(liquidation_price))  
+        print('STOPPX = ', int(trailing_stop))
         client.Order.Order_new(symbol='XBTUSD', 
                                ordType='Stop', 
                                orderQty=size, 
                                stopPx=int(trailing_stop), 
                                execInst='MarkPrice', 
                                clOrdID='short_stop').result() 
-        log_message(f'{last_price} {size} short trade opened')
+        log_message(f'{int(trailing_stop)} short stop set')
+
 
     if trade_status == 'in_long':
 
@@ -179,15 +211,9 @@ while True:
             # raise long trailing stop level
             trailing_stop = max(lower_bound, float(liquidation_price) * 1.1)
             client.Order.Order_amend(origClOrdID='long_stop', stopPx=int(trailing_stop)).result()
-            log_message(f'{trailing_stop} long stop raised')
-
-        if lowest_ask < float(trailing_stop):  
-            # position was closed by long trailing stop
-            trade_status = 'none'
-            log_message(f'{last_price} long trade stopped')  
+            log_message(f'{int(trailing_stop)} long stop raised')
 
         if lower_bound < float(liquidation_price) * 1.1:
-            print('MAX_RISK is too high')
             log_message('reduce max risk')
 
     if trade_status == 'in_short':
@@ -196,24 +222,14 @@ while True:
             # reduce short trailing stop level
             trailing_stop = min(upper_bound, float(liquidation_price) / 1.1)
             client.Order.Order_amend(origClOrdID='short_stop', stopPx=int(trailing_stop)).result() 
-            log_message(f'{trailing_stop} short stop lowered')
-
-        if highest_bid > float(trailing_stop):
-            # position was closed by short trailing stop 
-            trade_status = 'none'
-            log_message(f'{last_price} short trade stopped')
+            log_message(f'{int(trailing_stop)} short stop lowered')
 
         if upper_bound > liquidation_price / 1.1:
-            print('MAX_RISK is too high')
             log_message('reduce max risk')
-
-    with open('trading_algorithm/persisted/trade_status.txt', 'w') as file:
-        # write to volume in case container restarts
-        file.write(trade_status)
 
     if trade_status != 'none':
 
-        with open('trading_algorithm/persisted/trailing_stop.txt', 'w') as file:
+        with open('trading_algorithm/persisted/trailing_stop.txt', 'w') as file:    # container needs volume mount
             # write to volume in case container restarts
             file.write(str(trailing_stop))    
 
